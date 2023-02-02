958c3af67fd79415ee2b3687181bfc6700da7e66
Karlis <lacitiskarlis@gmail.com>
2023-02-02 17:22:57 +0200

    Added local storage rendering in home

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <p>Home</p>;
+  let taskNameValue = [];
+  let descriptionValue = [];
+  let dateValue = [];
+  let htmlCode = "";
+
+  let taskObj;
+  for (let i = 0; i < localStorage.length; i++) {
+    taskObj = JSON.parse(localStorage.getItem(i + 1));
+    taskNameValue[i] = taskObj.taskName;
+    descriptionValue[i] = taskObj.description;
+    dateValue[i] = taskObj.date;
+    htmlCode += `<div><h1>${taskNameValue[i]}</h1></br><p>${descriptionValue[i]}</p></br><p>${dateValue[i]}</p></div>`;
+  }
+
+  return <div dangerouslySetInnerHTML={{ __html: htmlCode }} />;
 }
